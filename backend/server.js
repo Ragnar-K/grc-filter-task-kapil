883cfc5cc78e5f3ba6f001f5cf3b3e389cfcb30d
Ragnar-K <kapil13kay@gmail.com>
2026-02-14 01:13:00 +0530

    url update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://grc-filter-task-kapil-frontend.onrender.com/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
